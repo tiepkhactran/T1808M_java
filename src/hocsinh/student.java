86eae8c41843c63b4fdf67eda1ab87d2ecc9e5ad
tiepkhactran <tiepkhactran@users.noreply.github.com>
2019-03-12 20:25:17 +0700

    sess1

---
+++ src/hocsinh/student.java
@@ -0,0 +1,45 @@
+package hocsinh;
+import java.util.Scanner;
+class Student {
+    public String MaSinhVien;
+    public float DiemTB;
+    public int Tuoi;
+    public String Lop;
+
+    public void inputInfo() {
+        Scanner add = new Scanner(System.in);
+        System.out.println("MaSinhVien: ");
+        MaSinhVien = add.nextLine();
+
+        System.out.println("DiemTB: ");
+        DiemTB = add.nextFloat();
+
+        System.out.println("Tuoi: ");
+        Tuoi = add.nextInt();
+        Lop = add.nextLine();
+        System.out.println("Lop: ");
+        Lop = add.nextLine();
+    }
+        public void showInfo () {
+            System.out.println("Thong tin sinh vien");
+            System.out.println("MaSinhVien: " + MaSinhVien);
+            System.out.println("DiemTB: " + DiemTB);
+            System.out.println("Tuoi: " + Tuoi);
+            System.out.println("Lop: " + Lop);
+        }
+            public void xetHB () {
+                if (DiemTB > 8.0) {
+                    System.out.println("Dat hoc bong");
+                } else {
+                    System.out.println("Khong dat hoc bong");
+                }
+            }
+
+                public static void main(String[]agrs){
+                    Student HocSinh = new Student();
+                    HocSinh.inputInfo();
+                    HocSinh.showInfo();
+                    HocSinh.xetHB();
+
+                }
+            }
