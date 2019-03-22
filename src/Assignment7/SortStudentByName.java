36959700b62a6810c9f1fe48d85171690802dc4e
tiepkhactran <tiepkhactran@users.noreply.github.com>
2019-03-22 23:23:10 +0700

    Assignment7

---
+++ src/Assignment7/SortStudentByName.java
@@ -0,0 +1,17 @@
+package Assignment7;
+
+
+import java.util.Comparator;
+
+/**
+ * SortStudentByName class
+ *
+ * @author viettuts.vn
+ */
+public class SortStudentByName implements Comparator<Student> {
+    @Override
+    public int compare(Student student1, Student student2) {
+        return student1.getName().compareTo(student2.getName());
+    }
+}
+
