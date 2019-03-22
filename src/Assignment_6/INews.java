7da4ab6011aec8c76f4a0bd0aa1d5954112f79fc
tiepkhactran <tiepkhactran@users.noreply.github.com>
2019-03-22 08:03:06 +0700

    Assignment6

---
+++ src/Assignment_6/INews.java
@@ -0,0 +1,7 @@
+package Assignment_6;
+
+public interface INews {
+    public void Display(){
+        System.out.println();
+    }
+}
---
+++ src/Assignment_6/News.java
@@ -0,0 +1,58 @@
+package Assignment_6;
+
+public class News {
+    public int ID;
+    public String Title;
+    public String PublishDate;
+    public String Author;
+    public String Content;
+    public float AverageRate;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getPublishDate() {
+        return PublishDate;
+    }
+
+    public void setPublishDate(String publishDate) {
+        PublishDate = publishDate;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(String author) {
+        Author = author;
+    }
+
+    public String getContent() {
+        return Content;
+    }
+
+    public void setContent(String content) {
+        Content = content;
+    }
+
+    public float getAverageRate() {
+        return AverageRate;
+    }
+    public class News interface INews{
+        ầhhaduhf
+
+    }
+}
